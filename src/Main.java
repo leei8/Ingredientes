ac526b20828f7802a89e5e6cf84d9753f6054287
ikaslea <ikaslea@gela07_01>
2017-03-10 09:31:49 +0100

    Mensajes de solucion implementados. Limpiar formulario implementado. Responsive

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import java.awt.Color;
 import javax.swing.JRadioButton;
@@ -21,10 +23,39 @@ public class Main extends JFrame {
 
 	private JPanel contentPane;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private JButton botonComprobar;
+	private JCheckBox patataCB;
+	private JCheckBox pimientoCB;
+	private JCheckBox huevoCB;
+	private JCheckBox lechugaCB;
+	private JCheckBox cebollaCB;
+	private JCheckBox jamonCB;
+	private JCheckBox zanahoriaCB;
+	private JCheckBox chorizoCB;
 
 	/**
 	 * Launch the application.
 	 */
+	protected void limpiarFormulario () {
+		patataCB.setSelected(false); 
+		pimientoCB.setSelected(false);
+		huevoCB.setSelected(false); 
+		lechugaCB.setSelected(false); 
+		cebollaCB.setSelected(false); 
+		chorizoCB.setSelected(false); 
+		zanahoriaCB.setSelected(false); 
+		jamonCB.setSelected(false); 
+	}
+	
+	protected void solucionCorrecta() {
+		JOptionPane.showMessageDialog(getContentPane(), "Has acertado!! Eres un genio en la cocina.");
+			
+	}
+	protected void solucionErronea() {
+		JOptionPane.showMessageDialog(getContentPane(), "Has fallado!! :( Vuelve a intentarlo.");
+		
+	}
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -43,7 +74,7 @@ public class Main extends JFrame {
 	 */
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 510, 350);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -53,31 +84,40 @@ public class Main extends JFrame {
 		lblAdivinaLosIngredientes.setFont(new Font("Tahoma", Font.BOLD, 26));
 		
 		JRadioButton radiobotonTCC = new JRadioButton("Tortilla con cebolla");
+		radiobotonTCC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				limpiarFormulario();
+			}
+		});
 		buttonGroup.add(radiobotonTCC);
 		
 		JRadioButton radiobotonTSC = new JRadioButton("Tortilla sin cebolla");
+		radiobotonTSC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				limpiarFormulario();
+			}
+		});
 		buttonGroup.add(radiobotonTSC);
 		
 		JRadioButton radiobotonTFJ = new JRadioButton("Tortilla francesa con jamon");
+		radiobotonTFJ.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				limpiarFormulario();
+			}
+		});
+		
 		buttonGroup.add(radiobotonTFJ);
 		
-		JCheckBox patataCB = new JCheckBox("Patata");
+		patataCB = new JCheckBox("Patata");
+		pimientoCB = new JCheckBox("Pimiento");
+		huevoCB = new JCheckBox("Huevo");
+		lechugaCB = new JCheckBox("Lechuga");
+		cebollaCB = new JCheckBox("Cebolla");
+		chorizoCB = new JCheckBox("Chorizo");
+		zanahoriaCB = new JCheckBox("Zanahoria");
+		jamonCB = new JCheckBox("Jamon");
 		
-		JCheckBox pimientoCB = new JCheckBox("Pimiento");
-		
-		JCheckBox huevoCB = new JCheckBox("Huevo");
-		
-		JCheckBox lechugaCB = new JCheckBox("Lechuga");
-		
-		JCheckBox cebollaCB = new JCheckBox("Cebolla");
-		
-		JCheckBox chorizoCB = new JCheckBox("Chorizo");
-		
-		JCheckBox zanahoriaCB = new JCheckBox("Zanahoria");
-		
-		JCheckBox jamonCB = new JCheckBox("Jamon");
-		
-		JButton botonComprobar = new JButton("Comprobar");
+		botonComprobar = new JButton("Comprobar");
 		botonComprobar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if (radiobotonTCC.isSelected()){
@@ -90,9 +130,9 @@ public class Main extends JFrame {
 						&& !zanahoriaCB.isSelected() 
 						&& !jamonCB.isSelected())
 					{
-						botonComprobar.setBackground(Color.GREEN);
+						solucionCorrecta();
 					} else {
-						botonComprobar.setBackground(Color.RED);
+						solucionErronea();
 					}
 				} else if (radiobotonTSC.isSelected()){
 					if (patataCB.isSelected() 
@@ -103,11 +143,11 @@ public class Main extends JFrame {
 						&& !chorizoCB.isSelected()
 						&& !zanahoriaCB.isSelected() 
 						&& !jamonCB.isSelected())
-						{
-							botonComprobar.setBackground(Color.GREEN);
-						} else {
-							botonComprobar.setBackground(Color.RED);
-						}
+					{
+						solucionCorrecta();
+					} else {
+						solucionErronea();
+					}
 					
 				} else if (radiobotonTFJ.isSelected()){
 					if (!patataCB.isSelected() 
@@ -118,11 +158,11 @@ public class Main extends JFrame {
 							&& !chorizoCB.isSelected()
 							&& !zanahoriaCB.isSelected() 
 							&& jamonCB.isSelected())
-						{
-							botonComprobar.setBackground(Color.GREEN);
-						} else {
-							botonComprobar.setBackground(Color.RED);
-						}
+					{
+						solucionCorrecta();
+					} else {
+						solucionErronea();
+					}
 				}
 			}
 		});
@@ -133,28 +173,43 @@ public class Main extends JFrame {
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addContainerGap()
-							.addComponent(lblAdivinaLosIngredientes, GroupLayout.DEFAULT_SIZE, 404, Short.MAX_VALUE))
+							.addComponent(lblAdivinaLosIngredientes, GroupLayout.DEFAULT_SIZE, 464, Short.MAX_VALUE))
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addGap(112)
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(patataCB)
-								.addComponent(pimientoCB)
-								.addComponent(huevoCB)
-								.addComponent(lechugaCB))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(patataCB, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addGap(8))
+								.addComponent(pimientoCB, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(huevoCB, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addGap(8))
+								.addComponent(lechugaCB, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 							.addGap(36)
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(cebollaCB)
-								.addComponent(jamonCB)
-								.addComponent(zanahoriaCB)
-								.addComponent(chorizoCB)))
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-							.addComponent(botonComprobar)
-							.addGroup(gl_contentPane.createSequentialGroup()
-								.addComponent(radiobotonTCC)
-								.addGap(18)
-								.addComponent(radiobotonTSC)
-								.addGap(18)
-								.addComponent(radiobotonTFJ))))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(cebollaCB, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addGap(12))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(jamonCB, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addGap(16))
+								.addComponent(zanahoriaCB, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(chorizoCB, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addGap(12)))
+							.addGap(188))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addContainerGap(328, Short.MAX_VALUE)
+									.addComponent(botonComprobar))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(radiobotonTCC, GroupLayout.DEFAULT_SIZE, 113, Short.MAX_VALUE)
+									.addGap(18)
+									.addComponent(radiobotonTSC, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addGap(18)
+									.addComponent(radiobotonTFJ, GroupLayout.DEFAULT_SIZE, 155, Short.MAX_VALUE)))
+							.addGap(61)))
 					.addContainerGap())
 		);
 		gl_contentPane.setVerticalGroup(
@@ -164,29 +219,31 @@ public class Main extends JFrame {
 					.addComponent(lblAdivinaLosIngredientes, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
 					.addGap(18)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(radiobotonTCC)
-						.addComponent(radiobotonTSC)
-						.addComponent(radiobotonTFJ))
+						.addComponent(radiobotonTCC, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(radiobotonTSC, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(radiobotonTFJ, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 					.addGap(26)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(patataCB)
-						.addComponent(cebollaCB))
+						.addComponent(patataCB, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(cebollaCB, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(pimientoCB)
-						.addComponent(chorizoCB))
+						.addComponent(chorizoCB, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(huevoCB)
-						.addComponent(zanahoriaCB))
+						.addComponent(zanahoriaCB, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lechugaCB)
-						.addComponent(jamonCB))
+						.addComponent(lechugaCB, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(jamonCB, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addComponent(botonComprobar)
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addGap(59))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
+
+
 }
